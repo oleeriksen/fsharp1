c1330b91108ada96d0d447cb2f0c71b5fe6de3b9
Ole Eriksen <oe@easv.dk>
2018-10-07 15:59:47 +0200

    init

---
+++ Console/Intro.fs
@@ -0,0 +1,101 @@
+﻿namespace Console
+
+module Intro =
+
+    // simple functions
+   let Inc a = a + 1
+
+   let Add a b = a + b
+
+   let Add2 (a,b) = a+b
+
+   let Add10 = Add 10
+
+   // The following 2 functions are equal - as functions
+
+   let AddAndInc = Add2 >> Inc;
+
+   let AddAndInc2 (a,b) = Inc( Add2 (a,b))
+
+
+
+
+
+
+   // function expressions
+
+   let DaysOfMonth = function
+        | 2 -> 28 // February
+        | 4 -> 30 //April
+        | 6 -> 30 //June
+        | 9 -> 30 // September
+        | 11 -> 30 // November
+        | _ -> 31
+
+   let DaysOfMonth2 = function
+        | 2 -> 28 // February
+        | 4|6|9|11 -> 30  //April
+        | _ -> 31
+
+   let rec fact = function
+      | 0 -> 1
+      | n when n > 0 -> n * fact(n-1)
+      | _ -> -1
+
+
+
+
+   
+   let rec power = function
+      | (x, 0) -> 1.0         
+      | (x, n) -> x * power(x,n-1)    
+   
+   let prefix = function
+       | true -> "Mr."
+       | false ->  "Ms."
+
+   let prefixNormal isMale = 
+           if isMale then "Mr."
+                     else  "Ms."
+
+   let SignAsText n = match n with
+                      | t when t > 0 -> "Positive"
+                      | 0 -> "Zero"
+                      | _ -> "Negative"   
+
+
+
+
+
+
+
+
+   // functions as parameters
+
+   let AddS prefixStr baseStr = prefixStr + " " +  baseStr  
+
+   let exclaim s = s + "!"
+
+
+   let names = ["Peter"; "Betty"; "Donald"; "Susan"; "Chris"]
+
+   let gender = [true; false; true; false; true]
+
+   // how about making the list["Mr. Peter!", "Ms. Betty!", ... ??
+   // each name prefixed with their gender and added a '!' to the end...
+
+
+
+
+   let namesWithEx = List.map (fun s -> exclaim s) names
+
+   let fully  = List.map2 (fun x y -> exclaim (AddS (prefix x)  y) ) gender names
+
+   let fully2 = List.map2 (fun x y -> AddS (prefix x)  y |>  exclaim ) gender names
+
+   // we can write out with prefix
+   let output = List.iter2 (fun x y -> printf "%s %s !" (prefix x) y) gender names 
+
+
+
+   
---
+++ Console/Program.fs
@@ -0,0 +1,47 @@
+﻿namespace Console
+open System
+module Main =
+
+
+    (* This is a multi-line comment *)
+    // This is a single-line comment
+
+    let a = 10
+
+    let IsEven n = (n % 2 = 0)
+
+    let rec AList n =
+        if n = 0 then []
+        else  AList (n-1) @  [n]
+             
+    let IsPrime n = [2..n-1] |> Seq.forall(fun x -> printf "%d " x; n % x <> 0)
+
+    let rec AListWithFilter n filter =
+            if n = 0 then []
+            elif  filter n then (AListWithFilter (n-1) filter) @ [n]
+            else AListWithFilter (n-1) filter
+
+    // exercise: solve exercise 1 from compulsory assignment in Contracts. 
+
+    let TrueDivisors n = [2..n-1] |> List.filter (fun d -> n % d = 0)
+
+    let rec TD2 n max =
+            if n = 2 then []
+            elif  max % n = 0 then (TD2 (n-1) max) @ [n]
+            else TD2 (n-1) max
+
+    let TrueDivisors2 n =  TD2 (n/2) n
+
+    let main arg =
+        //Console.WriteLine("Add7 4 = {0}", Example1.Add7 4)
+        //Console.WriteLine("summa(4,2)= {0}", (Summa (4, 2)) );
+        printfn "%s siger: De lige tal mellem 1 og 10 %A" arg (AListWithFilter 10 IsEven)
+       
+       //Console.WriteLine("sign {0}: {1}", Add 5, (sign (Add 5) ))
+       //(AListExtended 10 IsEven) |> Seq.iter (fun x -> printf "%d " x);
+        //(AListWithFilter 100 IsPrime) |> Seq.iter (fun x -> printf "%d " x)
+        printfn "Primtal mellem 2 og 100 %A" (AListWithFilter 100 IsPrime) 
+       //[1..10] |> Seq.iter (fun x -> printf "%d isEven %b " x (IsEven x))
+      
+    [<EntryPoint>]
+    main "peter"
